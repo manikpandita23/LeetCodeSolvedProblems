958afebb8461ed9effca04715e10069efbcf1526
Manik Pandita <115569552+manikpandita23@users.noreply.github.com>
2024-02-17 22:30:18 +0530

    Create DesignAFoodRatingSystem.py

---
+++ DesignAFoodRatingSystem.py
@@ -0,0 +1,49 @@
+class Food:
+    def __init__(self, food_rating, food_name):
+        # Store the food's rating.
+        self.food_rating = food_rating
+        # Store the food's name.
+        self.food_name = food_name
+
+    def __lt__(self, other):
+        # Overload the less than operator for comparison.
+        # If food ratings are the same, sort based on their name (lexicographically smaller name food will be on top).
+        if self.food_rating == other.food_rating:
+            return self.food_name < other.food_name
+        # Sort based on food rating (bigger rating food will be on top).
+        return self.food_rating > other.food_rating
+
+class FoodRatings:
+    def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
+       
+        self.food_rating_map = {}
+        
+        self.food_cuisine_map = {}
+        
+        self.cuisine_food_map = defaultdict(list)
+
+        for i in range(len(foods)):
+           
+            self.food_rating_map[foods[i]] = ratings[i]
+            self.food_cuisine_map[foods[i]] = cuisines[i]
+           
+            heapq.heappush(self.cuisine_food_map[cuisines[i]], Food(ratings[i], foods[i]))
+
+    def changeRating(self, food: str, newRating: int) -> None:
+        
+        self.food_rating_map[food] = newRating
+        
+        cuisineName = self.food_cuisine_map[food]
+        heapq.heappush(self.cuisine_food_map[cuisineName], Food(newRating, food))
+
+    def highestRated(self, cuisine: str) -> str:
+        
+        highest_rated = self.cuisine_food_map[cuisine][0]
+
+       
+        while self.food_rating_map[highest_rated.food_name] != highest_rated.food_rating:
+            heapq.heappop(self.cuisine_food_map[cuisine])
+            highest_rated = self.cuisine_food_map[cuisine][0]
+
+      
+        return highest_rated.food_name
