85767b434a7f2be09f28e890248c4515c428df8f
Manik Pandita <115569552+manikpandita23@users.noreply.github.com>
2024-07-30 00:24:06 +0530

    Update Pow (x,n).ts

--- Pow (x,n).ts
+++ Pow (x,n).ts
@@ -1,7 +1,3 @@
 function myPow(x: number, n: number): number {
-  let result = 1;
-  for (let i = 0; i < Math.abs(n); i++) {
-    result *= x;
-  }
-  return n < 0 ? 1 / result : result;
+    return Math.pow(x,n)
 };
