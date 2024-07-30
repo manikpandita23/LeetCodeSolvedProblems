db0efd596f996d0b6700da5126241a88d7d5a6c2
Manik Pandita <115569552+manikpandita23@users.noreply.github.com>
2024-07-31 00:58:37 +0530

    Create Climbing Stairs.py

---
+++ Climbing Stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        one_step_before = 2
+        two_steps_before = 1
+        for i in range(3, n + 1):
+            current = one_step_before + two_steps_before
+            two_steps_before = one_step_before
+            one_step_before = current
+        
+        return one_step_before
