b8928b62bb37091dbea449ad447f012bc20df931
Manik Pandita <115569552+manikpandita23@users.noreply.github.com>
2024-07-31 00:56:49 +0530

    Create Sqrt(x).py

---
+++ Sqrt(x).py
@@ -0,0 +1,17 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        left, right = 1, x
+        result = 0
+        while left <= right:
+            mid = (left + right) // 2
+            mid_squared = mid * mid
+            if mid_squared == x:
+                return mid
+            elif mid_squared < x:
+                result = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        return result
