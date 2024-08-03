0aace1c6167c2b992a1dae9ffb0f4e4d91a30e0b
Manik Pandita <115569552+manikpandita23@users.noreply.github.com>
2024-08-03 22:06:32 +0530

    Create Make two Arrays Equal by reversing sub arrays.cpp

---
+++ Make two Arrays Equal by reversing sub arrays.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    bool canBeEqual(std ::vector<int>& target, std:: vector<int>& arr) {
+        std:: sort(target.begin(), target.end());
+        std:: sort(arr.begin(), arr.end());
+        return target ==arr;
+        
+    }
+};
