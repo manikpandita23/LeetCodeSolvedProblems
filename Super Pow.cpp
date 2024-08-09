0019dc690353fe6e5eb81a27e53bc8ab6a8978c4
Manik Pandita <115569552+manikpandita23@users.noreply.github.com>
2024-08-09 19:12:53 +0530

    Create Super Pow.cpp

---
+++ Super Pow.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    const int MOD = 1337;
+
+    int modPow(int a, int b) {
+        int result = 1;
+        a %= MOD;
+        while (b > 0) {
+            if (b % 2 == 1) {
+                result = (result * a) % MOD;
+            }
+            a = (a * a) % MOD;
+            b /= 2;
+        }
+        return result;
+    }
+
+    int superPow(int a, vector<int>& b) {
+        int result = 1;
+        for (int digit : b) {
+            result = modPow(result, 10) * modPow(a, digit) % MOD;
+        }
+        return result;
+    }
+};
